c0078b6041a57f12cafa1b66bb3508244a5aa9de
Anja Laube <mail@anjalaube.com>
2020-12-09 11:41:23 +0100

    first lines of method written

--- hangman.js
+++ hangman.js
@@ -1,20 +1,40 @@
+// Excersise
+// method to make a guess
+// 1. only accepts characters
+// 2. should add unique geuessed to guessed letters array
+// 3. should decrement the guesses Left if a unique guess isnt a match
+
 const Hangman = function (word, possibleGuesses) {
   this.word = word.toLowerCase().split('')
   this.possibleGuesses = possibleGuesses
-  this.guessedLetters = ['a','s']
+  this.guessedLetters = []
 }
 
-Hangman.prototype.getPuzzle = function () {
-  let puzzle = ''
 
-  this.word.forEach((letter) => {
-    if ( this.guessedLetters.includes(letter) || letter === '') {
-      puzzle += letter
-    } else {
-      puzzle += '*'
+Hangman.prototype.makeGuess = function () {
+  // 1. only accepts characters
+  const guess = ''
+
+  if (guess !== '') {
+  console.log('Guess ignored. Type letters only.')
+
+  } else {
+    Hangman.prototype.getPuzzle = function () {
+      let puzzle = ''
+
+      this.word.forEach((letter) => {
+        if ( this.guessedLetters.includes(letter) || letter === '') {
+          puzzle += letter
+          this.guessedLetters.push(letter)
+        } else {
+          puzzle += '*'
+          this.possibleGuesses -= 1
+        }
+      })
+
+      return puzzle
     }
-  })
-  return puzzle
+  }
 }
 
 const game1 = new Hangman("playground", 5)
