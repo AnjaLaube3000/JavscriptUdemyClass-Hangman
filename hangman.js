6b4dfbd3028f48b2538af1f6b8fd75c3947c3295
Anja Laube <mail@anjalaube.com>
2020-12-10 12:11:00 +0100

    Guess method working

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 // Excersise
 // method to make a guess
 // 1. only accepts characters
-// 2. should add unique geuessed to guessed letters array
+// 2. should add unique guesses to guessed letters array
 // 3. should decrement the guesses Left if a unique guess isnt a match
 
 const Hangman = function (word, possibleGuesses) {
@@ -10,38 +10,52 @@ const Hangman = function (word, possibleGuesses) {
   this.guessedLetters = []
 }
 
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = ''
 
-Hangman.prototype.makeGuess = function () {
-  // 1. only accepts characters
-  const guess = ''
-
-  if (guess !== '') {
-  console.log('Guess ignored. Type letters only.')
-
-  } else {
-    Hangman.prototype.getPuzzle = function () {
-      let puzzle = ''
-
-      this.word.forEach((letter) => {
-        if ( this.guessedLetters.includes(letter) || letter === '') {
-          puzzle += letter
-          this.guessedLetters.push(letter)
-        } else {
-          puzzle += '*'
-          this.possibleGuesses -= 1
-        }
-      })
-
-      return puzzle
+  this.word.forEach((letter) => {
+    if (this.guessedLetters.includes(letter) || letter === '') {
+      puzzle += letter
+    } else {
+      puzzle += '*'
     }
+  })
+  // if (this.possibleGuesses > 0) {
+  //   console.log(`Remaining guesses :${this.possibleGuesses}`)
+  // } else {
+  //   console.log('Game over!')
+  // }
+
+  return puzzle
+}
+
+Hangman.prototype.makeGuess = function (guess) {
+
+  guess = guess.toLowerCase()
+  const isUnique = !this.guessedLetters.includes(guess)
+  const isBadGuess = !this.word.includes(guess)
+
+  if (isUnique) {
+    this.guessedLetters.push(guess)
+  } else {
+    console.log(`You already guessed ${guess}. Try again.`)
   }
+
+  if (isUnique && isBadGuess) {
+    this.possibleGuesses--
+  }
+
+
 }
 
 const game1 = new Hangman("playground", 5)
-console.log(game1)
+game1.makeGuess('l')
+game1.makeGuess('x')
+game1.makeGuess('a')
 console.log(game1.getPuzzle())
+console.log(game1.possibleGuesses)
 
 
-const game2 = new Hangman('Kleiderschrank', 7)
-console.log(game2)
-console.log(game2.getPuzzle())
+// const game2 = new Hangman('Kleiderschrank', 7)
+// console.log(game2)
+// console.log(game2.getPuzzle())
